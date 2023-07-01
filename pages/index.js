c2582acadea8d1a53fff61a5bd0e42e48fcbf90d
ananduremanan <48993261+ananduremanan@users.noreply.github.com>
2023-07-01 17:07:56 +0530

    1 file modified, 1 added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import Gallery from './components/Gallery';
 import Nav from './components/Nav';
 
 export default function Home() {
@@ -8,68 +9,7 @@ export default function Home() {
       <main>
         <div className="bg-gray-800 p-4 md:h-screen lg:px-40 lg:h-screen">
           <Nav />
-          <div className="grid grid-cols-1 gap-4 bg-gray-800 md:grid-cols-2 lg:grid-cols-4">
-            <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow-md hover:shadow-lg">
-              <img
-                src="https://cdn.jsdelivr.net/gh/ananduremanan/dreamCatcherNext@main/pages/assets/images/OIG%20(1)%201.jpg"
-                alt="fox"
-              />
-              <div className="m-4">
-                <p className="font-bold text-gray-500">The Room</p>
-                <p className="text-sm font-light text-gray-400">
-                  Dark Moody with minimal lighting.
-                </p>
-              </div>
-            </div>
-            <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow-md hover:shadow-lg">
-              <img
-                src="https://cdn.jsdelivr.net/gh/ananduremanan/dreamCatcherNext@main/pages/assets/images/OIG%20(1).jpg"
-                alt="fox"
-              />
-              <div className="m-4">
-                <p className="font-bold text-gray-500">The Dream Catcher</p>
-                <p className="text-sm font-light text-gray-400">
-                  Made of out of feathers, sticks and curiosity.
-                </p>
-              </div>
-            </div>
-            <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow-md hover:shadow-lg">
-              <img
-                src="https://cdn.jsdelivr.net/gh/ananduremanan/dreamCatcherNext@main/pages/assets/images/OIG.jpg"
-                alt="fox"
-              />
-              <div className="m-4">
-                <p className="font-bold text-gray-500">The Catcher</p>
-                <p className="text-sm font-light text-gray-400">
-                  Who's he? Where is he from? No one knows!.
-                </p>
-              </div>
-            </div>
-            <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow-md hover:shadow-lg">
-              <img
-                src="https://cdn.jsdelivr.net/gh/ananduremanan/dreamCatcherNext@main/pages/assets/images/OIG._gqO.jpg"
-                alt="fox"
-              />
-              <div className="m-4">
-                <p className="font-bold text-gray-500">The Room In a different angle</p>
-                <p className="text-sm font-light text-gray-400">
-                  Image Generated with Dall-E 2.
-                </p>
-              </div>
-            </div>
-            <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow-md hover:shadow-lg">
-              <img
-                src="https://stackblitz.com/files/stackblitz-starters-vt4zdd/github/ananduremanan/dreamCatcherNext/main/pages/assets/images/car_accident.jpg"
-                alt="fox"
-              />
-              <div className="m-4">
-                <p className="font-bold text-gray-500">The Accident</p>
-                <p className="text-sm font-light text-gray-400">
-                  Image Generated with Dall-E 2.
-                </p>
-              </div>
-            </div>
-          </div>
+          <Gallery />
         </div>
       </main>
     </div>
