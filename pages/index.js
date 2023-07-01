3267c566d072c49b17a91ae1048921b253ecaa9a
ananduremanan <48993261+ananduremanan@users.noreply.github.com>
2023-07-01 15:42:56 +0530

    3 files modified, 1 added

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import room from './assets/images/room.jpg';
+import Nav from './components/Nav';
 
 export default function Home() {
   return (
     <div>
+      <Nav />
       <main>
         <div className="bg-gray-800 p-4 md:h-screen">
           <div className="grid grid-cols-1 gap-4 bg-gray-800 md:grid-cols-4">
-            <div className="cursor-pointer rounded-lg bg-white shadow-md hover:shadow-lg">
+            <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow-md hover:shadow-lg">
               <Image src={room} alt="fox" />
               <div className="m-4">
                 <p className="font-bold text-gray-500">The Cunning</p>
