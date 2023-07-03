2b2c4ca8ab4537f10ed9db2cc0fb3392f61b4381
ananduremanan <48993261+ananduremanan@users.noreply.github.com>
2023-07-03 15:15:26 +0530

    3 files modified, 3 added

--- pages/components/Gallery.js
+++ pages/components/Gallery.js
@@ -1,30 +1,67 @@
+import { useState } from 'react';
 import img_url from '../data/imageUrl.json';
+import { Modal } from './gallery-modal/Modal';
 
 export default function Gallery() {
+  const [selectedImg, setSelectedImg] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleImgClick = (imgSrc) => {
+    setSelectedImg(imgSrc);
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setSelectedImg(null);
+    setIsModalOpen(false);
+  };
+
+  const handleNext = () => {
+    const currentIndex = img_url.findIndex((url) => url.img_url === selectedImg);
+    const nextIndex = (currentIndex + 1) % img_url.length;
+    setSelectedImg(img_url[nextIndex].img_url);
+  };
+
+  const handlePrev = () => {
+    const currentIndex = img_url.findIndex((url) => url.img_url === selectedImg);
+    const prevIndex = (currentIndex - 1 + img_url.length) % img_url.length;
+    setSelectedImg(img_url[prevIndex].img_url);
+  };
+
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-      {img_url.map((url) => {
-        return (
-          <div
-            className="cursor-pointer overflow-hidden rounded-lg bg-white shadow-md hover:shadow-lg"
-            key={url.id}
-          >
-            <div className="overflow-hidden">
-              <img
-                src={url.img_url}
-                alt="fox"
-                className="transform transition-transform duration-300 hover:scale-110"
-              />
+    <>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+        {img_url.map((url) => {
+          return (
+            <div
+              className="cursor-pointer overflow-hidden rounded-lg bg-white shadow-md hover:shadow-lg"
+              key={url.id}
+              onClick={() => handleImgClick(url.img_url)}
+            >
+              <div className="overflow-hidden">
+                <img
+                  src={url.img_url}
+                  alt="fox"
+                  className="transform transition-transform duration-300 hover:scale-110"
+                />
+              </div>
+              <div className="m-4">
+                <p className="font-bold text-gray-500">{url.title}</p>
+                <p className="text-sm font-light text-gray-400">
+                  {url.description}
+                </p>
+              </div>
             </div>
-            <div className="m-4">
-              <p className="font-bold text-gray-500">{url.title}</p>
-              <p className="text-sm font-light text-gray-400">
-                {url.description}
-              </p>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={handleModalClose}
+        imgSrc={selectedImg}
+        onNext={handleNext}
+        onPrev={handlePrev}
+      />
+    </>
   );
 }
---
+++ pages/components/gallery-modal/Modal.js
@@ -0,0 +1,35 @@
+export function Modal({ isOpen, onClose, imgSrc, onNext, onPrev }) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-50">
+      <div className="relative w-3/4 h-3/4">
+        <div className="flex items-center justify-center w-full h-full">
+          <img
+            src={imgSrc}
+            alt="Full screen"
+            className="max-w-full max-h-full rounded-lg"
+          />
+        </div>
+        <button
+          onClick={onClose}
+          className="absolute top-0 right-0 p-2 text-white bg-black rounded-full"
+        >
+          X
+        </button>
+        <button
+          onClick={onPrev}
+          className="absolute left-0 top-1/2 p-2 text-white bg-black rounded-full"
+        >
+          &lt;
+        </button>
+        <button
+          onClick={onNext}
+          className="absolute right-0 top-1/2 p-2 text-white bg-black rounded-full"
+        >
+          &gt;
+        </button>
+      </div>
+    </div>
+  );
+}
