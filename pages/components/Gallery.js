36bce4a8007960a16dc727de4dc3d409fe70148a
ananduremanan <48993261+ananduremanan@users.noreply.github.com>
2023-07-03 14:41:02 +0530

    2 files modified, 1 added

--- pages/components/Gallery.js
+++ pages/components/Gallery.js
@@ -1,80 +1,30 @@
+import img_url from '../data/imageUrl.json';
+
 export default function Gallery() {
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-      <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow-md hover:shadow-lg">
-        <div className="overflow-hidden">
-          <img
-            src="https://cdn.jsdelivr.net/gh/ananduremanan/dreamCatcherNext@main/pages/assets/images/OIG%20(1)%201.jpg"
-            alt="fox"
-            className="transform transition-transform duration-300 hover:scale-110"
-          />
-        </div>
-        <div className="m-4">
-          <p className="font-bold text-gray-500">The Room</p>
-          <p className="text-sm font-light text-gray-400">
-            Dark Moody with minimal lighting.
-          </p>
-        </div>
-      </div>
-      <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow-md hover:shadow-lg">
-        <div className="overflow-hidden">
-          <img
-            src="https://cdn.jsdelivr.net/gh/ananduremanan/dreamCatcherNext@main/pages/assets/images/OIG%20(1).jpg"
-            alt="fox"
-            className="transform transition-transform duration-300 hover:scale-110"
-          />
-        </div>
-        <div className="m-4">
-          <p className="font-bold text-gray-500">The Dream Catcher</p>
-          <p className="text-sm font-light text-gray-400">
-            Made of out of feathers, sticks and curiosity.
-          </p>
-        </div>
-      </div>
-      <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow-md hover:shadow-lg">
-        <div className="overflow-hidden">
-          <img
-            src="https://cdn.jsdelivr.net/gh/ananduremanan/dreamCatcherNext@main/pages/assets/images/OIG.jpg"
-            alt="fox"
-            className="transform transition-transform duration-300 hover:scale-110"
-          />
-        </div>
-        <div className="m-4">
-          <p className="font-bold text-gray-500">The Catcher</p>
-          <p className="text-sm font-light text-gray-400">
-            Who's he? Where is he from? No one knows!.
-          </p>
-        </div>
-      </div>
-      <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow-md hover:shadow-lg">
-        <div className="overflow-hidden">
-          <img
-            src="https://cdn.jsdelivr.net/gh/ananduremanan/dreamCatcherNext@main/pages/assets/images/OIG._gqO.jpg"
-            alt="fox"
-            className="transform transition-transform duration-300 hover:scale-110"
-          />
-        </div>
-        <div className="m-4">
-          <p className="font-bold text-gray-500">
-            The Room In a different angle
-          </p>
-          <p className="text-sm font-light text-gray-400">
-            Image Generated with Dall-E 2.
-          </p>
-        </div>
-      </div>
-      {/* <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow-md hover:shadow-lg">
+      {img_url.map((url) => {
+        return (
+          <div
+            className="cursor-pointer overflow-hidden rounded-lg bg-white shadow-md hover:shadow-lg"
+            key={url.id}
+          >
+            <div className="overflow-hidden">
               <img
-                src="https://stackblitz.com/files/stackblitz-starters-vt4zdd/github/ananduremanan/dreamCatcherNext/main/pages/assets/images/car_accident.jpg"
+                src={url.img_url}
                 alt="fox"
+                className="transform transition-transform duration-300 hover:scale-110"
               />
-              <div className="m-4">
-                <p className="font-bold text-gray-500">The Accident</p>
-                <p className="text-sm font-light text-gray-400">
-                  Image Generated with Dall-E 2.
-                </p>
-              </div>
-            </div> */}
+            </div>
+            <div className="m-4">
+              <p className="font-bold text-gray-500">{url.title}</p>
+              <p className="text-sm font-light text-gray-400">
+                {url.description}
+              </p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       <main>
         <div className="bg-gray-800 p-4 mmd:min-h-screen lg:px-40 lg:min-h-screen">
           <Nav />
-          <div className="text-white font-light mt-8 text-center">
+          <div className="text-white font-light mt-8">
             The Ai Generated Images from the Short
           </div>
           <hr className="m-3 opacity-10" />
